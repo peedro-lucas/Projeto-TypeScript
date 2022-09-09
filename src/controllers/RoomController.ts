11d0d077ceacfdb177c21e15800a4207bcef0477
Pedro Lucas <pedro.lmarinho2002@gmail.com>
2022-09-09 05:44:23 -0300

    add: criar disciplina para aula

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { roomRepository } from "../repositories/roomRepository";
+import { subjectRepository } from "../repositories/subjectRepository";
+import { VideoRepository } from "../repositories/VideoRepository";
 
 export class RoomController{
     async create(req: Request, res: Response){
@@ -17,4 +19,59 @@ export class RoomController{
             return res.status(500).json({message: "Internal Server Error"})
         }
     }
+    async createVideo(req: Request, res: Response){
+        const {title, url} = req.body
+        const {idRoom} = req.params
+
+        try{
+            const room = await roomRepository.findOneBy({id: Number(idRoom)})
+
+            if(!room){
+                return res.status(404).json({message: 'Aula não existente'})
+            }
+            const newVideo = VideoRepository.create({title,url,room})
+
+            await VideoRepository.save(newVideo)
+
+            return res.status(201).json(newVideo)
+            
+        }catch(e){
+            console.log(e)
+            return res.status(500).json({message: "Internal Server Error"})
+        }
+    }
+
+    async roomSubject(req: Request, res: Response){
+        const { subject_id } = req.body
+        const { idRoom } = req.params
+
+        try{
+            const room = await roomRepository.findOneBy({id: Number(idRoom)})
+
+            if(!room){
+                return res.status(404).json({message: 'Aula não existente'})
+            }
+
+            const subject = await subjectRepository.findOneBy({id: Number(subject_id)})
+
+            if (!subject){
+                return res.status(404).json({message: 'Disciplina não existente'})
+            }
+
+            const roomUpdate = {
+                ...room,
+                subjects: [subject]
+            }
+
+            await roomRepository.save(roomUpdate)
+
+            return res.status(200).json(room)
+
+
+        }catch(e){
+            console.log(e)
+            return res.status(500).json({message: "Internal Server Error"})
+        }
+    }
+
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,13 @@
 import {Router} from 'express'
 import { RoomController } from './controllers/RoomController'
 import { SubjectController } from './controllers/SubjectController'
-import { VideoController } from './controllers/VideoController'
 
 const routes = Router()
 
 routes.post('/subject', new SubjectController().create)
 routes.post('/room', new RoomController().create)
-routes.post('/video', new VideoController().create)
+routes.post('/room/:idRoom/create',new RoomController().createVideo )
+routes.post('/room/:idRoom/subject',new RoomController().roomSubject )
 
 export default routes
 
